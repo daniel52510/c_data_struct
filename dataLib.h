a3fd737527efe6341370e026adb9612bff9f9c14
daniel52510 <danielblazquez640@gmail.com>
2023-03-04 12:22:06 -0500

    array DS beginning

--- dataLib.h
+++ dataLib.h
@@ -1,8 +1,20 @@
 //
 // Created by Daniel Blazquez on 3/4/23.
 //
+#include <iostream>
+using namespace std;
 
-#ifndef LEARN_C___DATALIB_H
-#define LEARN_C___DATALIB_H
-
-#endif //LEARN_C___DATALIB_H
+class Array {
+public:
+    int* size_ptr = &arr_size;
+    int arr_size;
+    int* arr = new int[arr_size];
+    void set_arr_size(int as) {
+        *size_ptr = as;
+        cout << "array memory address: " << &arr_size << endl;
+        cout << "array size value: " << arr_size << endl;
+     }
+    int get_arr_size() {
+        return sizeof(arr)/sizeof(int);
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,42 +1,7 @@
-#include <iostream>
-using namespace std;
-class Car {
-public:
-    string brand;
-    string model;
-    string type;
-    int year;
-    double weight;
-};
+#include "dataLib.h";
 int main() {
-    Car car1;
-    car1.brand = "BMW";
-    car1.model = "M3";
-    car1.type = "Coupe";
-    car1.year = 2017;
-    car1.weight = 2304.60;
-
-    string food = "Pizza";
-    string side = "French Fries";
-    cout << "Memory Address from food: " << &food << endl;
-    cout << "Memory Address from side: " << &side << endl;
-    //creating and using pointers
-    //This one pointer is pointing to the address
-    // of the food variable but it can be changed to point
-    // somewhere else.
-    string *ptr = &food;
-    cout << "Pointer Address: " << ptr << endl;
-    cout << "Pointer Value: " << *ptr << endl;
-    //This changes where the pointer is pointing to.
-    ptr = &side;
-    cout << "Current Pointer Address: " << ptr << endl;
-    cout << "Current Pointer Value: " << *ptr << endl;
-
-    *ptr = "Chicken Fingers";
-
-    cout << "Current Pointer Address: " << ptr << endl;
-    cout << "Current Pointer Value: " << *ptr << endl;
-
-    cout << car1.year << " " << car1.brand << " " << car1.model;
+    Array a;
+    a.set_arr_size(5);
+    cout << "array size: " <<  a.get_arr_size() << endl;
     return 0;
 }
