3136b714ad5b48da4d63afa003481c52e8ada92f
daniel52510 <danielblazquez640@gmail.com>
2023-03-06 18:43:57 -0500

    working delete function, now working on standard insertion sort for array

--- dataLib.h
+++ dataLib.h
@@ -40,7 +40,6 @@ public:
        }
        else {
            int new_size = *size_ptr;
-           //cout << "New Size: " << new_size << endl;
            int *temp = new int[new_size];
            for(int i = 0; i < new_size; i++) {
                temp[i] = arr[i];
@@ -50,5 +49,56 @@ public:
            arr[new_size] = x;
        }
         *size_ptr += 1;
+    }
+    void delete_arr(int num, int pos) {
+        if(*size_ptr == 0) {
+            cout << "Sorry, there is nothing to delete!" << endl;
+        } else {
+            cout << "Size in Delete Function I: " <<*size_ptr << endl;
+            for(int i = 0; i < *size_ptr; i++) {
+                if(arr[i] == num and i == pos) {
+                    *size_ptr = *size_ptr - 1;
+                    for(int j = i; j < *size_ptr; j++) {
+                        arr[j] = arr[j + 1];
+                    }
+                }
+            }
+            cout << "Size in Delete Function II: " <<*size_ptr << endl;
+            int *temp = new int[*size_ptr];
+            for(int k = 0; k < *size_ptr; k++){
+                temp[k] = arr[k];
+            }
+            arr = temp;
+        }
+    }
+    void insert_sort_arr() {
+        //grab the element in the one location (not zero)
+        //check variable(s) to the left, for each one that is greater,
+        //you shift the number to the right until the number is
+        //not greater than the left most element. You place it in front of it.
+       cout << "Starting Array: " << endl;
+        print_arr();
+       if(*size_ptr == 0 or *size_ptr == 1) {
+           cout << "Error, there is nothing to sort!" << endl;
+       }
+       else {
+           for(int i = 1; i < *size_ptr; i++) {
+               for(int j = i - 1; j >= 0; j--) {
+                   int temp = arr[i];
+                   if(arr[j] > temp){
+                       //shift array elements below
+                       temp = arr[j];
+                       arr[j] = arr[j + 1];
+
+                   }
+                   else {
+
+                   }
+               }
+           }
+           cout << "Ending Array: " << endl;
+           print_arr();
+       }
+
     }
 };
--- main.cpp
+++ main.cpp
@@ -1,16 +1,11 @@
 #include "dataLib.h";
 int main() {
     Array a;
-    a.print_arr();
+    a.add_arr(4);;
+     a.add_arr(1);
+     a.add_arr(7);
     a.add_arr(2);
-    a.print_arr();
-     a.add_arr(4);
-     a.print_arr();
-     a.add_arr(6);
-     a.print_arr();
-    a.add_arr(3);
-    a.print_arr();
     a.add_arr(21);
-    a.print_arr();
+    a.insert_sort_arr();
     return 0;
 }
